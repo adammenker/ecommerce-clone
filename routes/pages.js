d7f489cde5424816d7551ddd752e961b5fc41018
Adam <adam.j.menker@gmail.com>
2021-04-09 18:50:11 -0500

    troubleshooting port error

--- routes/pages.js
+++ routes/pages.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    try{
+        res.render('index');
+    } catch(error){
+        console.log(error);
+    }
 });
 
 router.get('/register', (req, res) => {
