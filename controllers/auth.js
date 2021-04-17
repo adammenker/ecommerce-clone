628b3485221771acac71029149fc476a07b26662
Adam <adam.j.menker@gmail.com>
2021-04-17 15:28:27 -0500

    updated db export

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const db = require("../util/database");
 
-const db = mysql.createPool({
-    host: "us-cdbr-east-03.cleardb.com",
-    user: "b04903d33dd1c0",
-    password: "d17038e0",
-    database: "heroku_ce5d691c17f624d"
-});
+// const db = mysql.createPool({
+//     host: "us-cdbr-east-03.cleardb.com",
+//     user: "b04903d33dd1c0",
+//     password: "d17038e0",
+//     database: "heroku_ce5d691c17f624d"
+// });
 
 
 exports.register = (req, res) => {
---
+++ util/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: mysql.createPool({
+        host: "us-cdbr-east-03.cleardb.com",
+        user: "b04903d33dd1c0",
+        password: "d17038e0",
+        database: "heroku_ce5d691c17f624d"
+    })
+}
