a4d1cbc2087d2acbda4fda3a1cfcafd405480e09
Adam <adam.j.menker@gmail.com>
2021-04-17 18:04:03 -0500

    cookies set

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
 
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log(`Server started on port ${process.env.PORT}`);
-// });
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
