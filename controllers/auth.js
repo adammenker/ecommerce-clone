c1f9435a8b480058f7b50bee2486b0098e454137
Adam <adam.j.menker@gmail.com>
2021-04-17 16:32:57 -0500

    troubleshooting login page creating db logic in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,19 +61,24 @@ exports.login = async (req, res) => {
         }
 
         db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) =>{
-            console.log(results);
-            if(!results || !(await bcrypt.compare(password, results[0].password))){
-                res.status(401).render('login', {
-                    message: 'Email or password is incorrect'
-                });
-            }else {
-                const id = results[0].id;
+            try{
+                console.log(results);
+                if(!results || !(await bcrypt.compare(password, results[0].password))){
+                    res.status(401).render('login', {
+                        message: 'Email or password is incorrect'
+                    });
+                }else {
+                    const id = results[0].id;
 
-                const token = jwt.sign({id}, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-                console.log(token);
+                    const token = jwt.sign({id}, process.env.JWT_SECRET, {
+                        expiresIn: process.env.JWT_EXPIRES_IN
+                    });
+                    console.log(token);
+                } 
+            } catch(error){
+                console.log(error);
             }
+            
         });
     } catch (err){
         console.log(err);
