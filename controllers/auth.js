135733c723e808b3f98c340ccf91871b4410bb72
Adam <adam.j.menker@gmail.com>
2021-04-17 15:45:39 -0500

    not using .env variables

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const db = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    host: "us-cdbr-east-03.cleardb.com",
+    user: "b04903d33dd1c0",
+    password: "d17038e0",
+    database: "heroku_ce5d691c17f624d"
 });
 
 
