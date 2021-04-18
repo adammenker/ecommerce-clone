b46ac23e9be21e1b1bb57d8bdbb40585cf18cf37
Adam <adam.j.menker@gmail.com>
2021-04-17 19:38:12 -0500

    testing middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const authController = require('../controllers/auth')
 const router = express.Router();
 
+console.log('\n\n\n\783468238973428,bkjdsa\n\n\n\n');
+
 router.post('/register', authController.register );
 
 router.post('/login', authController.login );
