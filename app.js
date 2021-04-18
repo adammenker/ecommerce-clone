e3fc2a22238e6d3b7753b5f77be36f67c1e9e0cc
Adam <adam.j.menker@gmail.com>
2021-04-18 14:14:02 -0500

    setting up profile page

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+// initialization
 const mysql = require("mysql");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config({path: './.env'});
-
-
 
 const app = express();
 const db = mysql.createPool({
@@ -14,16 +13,51 @@ const db = mysql.createPool({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
-
 const publicDirectory = path.join(__dirname, './public');
+
+// start of middleware
 app.use(express.static(publicDirectory));
 
 // parsing url & json
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// setting persistent login
+app.use(cookieParser());
+// app.use(
+//     session({
+//         key: 'user_sid',
+//         secret: process.env.JWT_SECRET,
+//         resave: false,
+//         saveUninitialized: false,
+//         cookie: {
+//             // one month
+//             expires: 2629800000
+//         }
+//     })
+// )
 
-// app.use(cookieParser());
+// app.use((req, res, next) => {
+//     if(req.session.user && req.cookies.user_sid){
+//         app.use('/homeLoggedIn', require('./routes/pages'));
+//     } else {
+//         next();
+//     }
+// });
+
+// const sessionChecker = (req, res, next) => {
+//     if(req.session.user && req.cookies.user_sid){
+//         app.use('/homeLoggedIn', require('./routes/pages'));
+//     } else {
+//         next();
+//     }
+// };
+
+// app.get('/', sessionChecker, (req, res) => {
+//     app.use('/auth', require('./routes/auth'));
+// });
+
+
 
 app.set('view engine', 'hbs');
 db.getConnection((error, connection) => {
