f73e8d6fa4fb55730ae58344ee9eb3283c86fe19
Adam <adam.j.menker@gmail.com>
2021-04-22 16:58:36 -0500

    user attributes validation: complete

--- public/generateCart.js
+++ public/generateCart.js
@@ -5,12 +5,19 @@ productText = productText.split("\\");
 
 for(let i = 0; i < productText.length; i++) {
     if(i == 0) {
+        // remove ending comma
         productText[i] = productText[i].slice(0, -1);
     } else {
+        // remove starting and ending comma
+        productText[i] = productText[i].slice(1);
         productText[i] = productText[i].slice(0, -1);
     }
 }
+// remove empty string at the end
+productText.slice(0, -1);
 
 console.log(productText);
+
+
 
 //document.getElementById("test2").innerHTML = "yeyeyw"
