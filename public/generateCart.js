86b6a3378cd10edbdd52588de7b19c5c4ee1886e
Adam <adam.j.menker@gmail.com>
2021-04-22 17:49:05 -0500

    testing generating more attributes in cart: added all attributes to be generated

--- public/generateCart.js
+++ public/generateCart.js
@@ -21,13 +21,21 @@ console.log(productArray);
 
 function createTableRows(productArr) {
     let tableBody = '';
+    let productName = '';
+    let productCategory = '';
+    let productPrice = '';
+    let currentProduct = ''
     for(let i = 0; i < productArr.length; i++) {
+        currentProduct = productArr.split(",");
+        productName = currentProduct[0];
+        productCategory = currentProduct[1];
+        productPrice = currentProduct[2];
         tableBody += 
             '<tr>' +
                 `<th scope="row">${'# ' + i}</th>` +
-                `<td>${productArray[i]}</td>` +
-                `<td>${'Otto'}</td>` +
-                `<td>${'$mdo'}</td>` +
+                `<td>${productName}</td>` +
+                `<td>${productCategory}</td>` +
+                `<td>${"$" + productPrice}</td>` +
             '</tr>'
     }
     return tableBody;
