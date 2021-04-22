7639f232e34dbe987ab2870f0f2efd328aac7edb
Adam <adam.j.menker@gmail.com>
2021-04-22 16:44:17 -0500

    testing string parsing

--- public/generateCart.js
+++ public/generateCart.js
@@ -3,3 +3,5 @@ let productText = document.getElementById("test").innerHTML
 console.log(productText);
 productText.split('\\');
 console.log(productText);
+
+document.getElementById("test2").innerHTML = "yeyeyw"
