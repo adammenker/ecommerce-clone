97aeb40b2269145774faf8ab7421a77a9ca12c02
Adam <adam.j.menker@gmail.com>
2021-04-22 16:50:26 -0500

    user attributes validation: complete

--- public/generateCart.js
+++ public/generateCart.js
@@ -1,7 +1,8 @@
 
 let productText = document.getElementById("test").innerHTML
+console.log(typeof productText);
 console.log(productText);
 productText.split("o");
 console.log(productText);
 
-document.getElementById("test2").innerHTML = "yeyeyw"
+//document.getElementById("test2").innerHTML = "yeyeyw"
