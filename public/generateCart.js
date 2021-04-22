4d80369f628a084e93c2aa0c7d270747aef086e3
Adam <adam.j.menker@gmail.com>
2021-04-22 16:54:55 -0500

    user attributes validation: complete

--- public/generateCart.js
+++ public/generateCart.js
@@ -1,8 +1,16 @@
 
 let productText = document.getElementById("test").innerHTML
-console.log(typeof productText);
-console.log(productText);
+
 productText = productText.split("\\");
+
+for(let i = 0; i < productText.length; i++) {
+    if(i == 0) {
+        productText[i] = productText[i].slice(0, -1);
+    } else {
+        productText[i] = productText[i].slice(0, -1);
+    }
+}
+
 console.log(productText);
 
 //document.getElementById("test2").innerHTML = "yeyeyw"
