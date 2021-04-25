807799320eb5f7d97a6caa06491d0a96968e9404
Adam <adam.j.menker@gmail.com>
2021-04-25 11:52:11 -0500

    generating order costs

--- public/generateCart.js
+++ public/generateCart.js
@@ -61,21 +61,3 @@ document.getElementById("quantity-label").innerHTML = `Quantity: ${quantity}`;
 document.getElementById("subtotal-label").innerHTML = `Subtotal: $${subtotal}`;
 
 document.getElementById("checkout-button").setAttribute("value", `$${subtotal},${quantity}`);
-
-function calculateTax(subtotal){
-    return subtotal * .08;
-}
-
-function calculateFinalTotal(tax, subtotal, shipping) { 
-    return tax + subtotal + shipping;
-}
-let shipping = 5
-let taxes = calculateTax(subtotal);
-let finalTotal = calculateFinalTotal(taxes, subtotal, shipping);
-
-if(document.getElementById("subtotal-price-header")) {
-    document.getElementById("subtotal-price-header").innerHTML = `Subtotal: $${subtotal}`;
-    document.getElementById("taxes-price-header").innerHTML = `Taxes: $${taxes}`;
-    document.getElementById("shipping-price-header").innerHTML = `Shipping: $${shipping}`;
-    document.getElementById("total-price-header").innerHTML = `Total: $${finalTotal}`;
-}
