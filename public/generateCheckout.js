e6058a3e001bbabff827e728d4e37c2252387558
Adam <adam.j.menker@gmail.com>
2021-04-25 12:01:57 -0500

    generating order costs

--- public/generateCheckout.js
+++ public/generateCheckout.js
@@ -5,17 +5,15 @@ function calculateTax(subtotal){
 }
 
 function calculateFinalTotal(tax, subtotal, shipping) { 
-    return tax + subtotal + shipping;
+    return parseInt(tax) + parseInt(subtotal) + parseInt(shipping);
 }
-let shipping = 5
+let shipping = 5;
 let taxes = calculateTax(subtotal).toFixed(2);
 let finalTotal = calculateFinalTotal(taxes, subtotal, shipping);
 
-if(document.getElementById("subtotal-price-header")) {
-    document.getElementById("subtotal-price-header").innerHTML = `Subtotal: $${subtotal}`;
-    document.getElementById("taxes-price-header").innerHTML = `Taxes: $${taxes}`;
-    document.getElementById("shipping-price-header").innerHTML = `Shipping: $${shipping}`;
-    document.getElementById("total-price-header").innerHTML = `Total: $${finalTotal}`;
-}
+document.getElementById("subtotal-price-header").innerHTML = `Subtotal: $${subtotal}`;
+document.getElementById("taxes-price-header").innerHTML = `Taxes: $${taxes}`;
+document.getElementById("shipping-price-header").innerHTML = `Shipping: $${shipping}`;
+document.getElementById("total-price-header").innerHTML = `Total: $${finalTotal}`;
 
 document.getElementById("hidden-price").setAttribute("opacity", 0);
