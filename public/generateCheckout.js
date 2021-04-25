df9f10718e50f1a92ff12ddd1e341ac0b1d66236
Adam <adam.j.menker@gmail.com>
2021-04-25 11:58:45 -0500

    generating order costs

--- public/generateCheckout.js
+++ public/generateCheckout.js
@@ -1,4 +1,4 @@
-let subtotal = document.getElementById("hidden-price").innerHTML;
+let subtotal = document.getElementById("hidden-price").innerHTML.toString();
 
 function calculateTax(subtotal){
     return subtotal * .08;
@@ -8,7 +8,7 @@ function calculateFinalTotal(tax, subtotal, shipping) {
     return tax + subtotal + shipping;
 }
 let shipping = 5
-let taxes = calculateTax(subtotal);
+let taxes = calculateTax(subtotal).toFixed(2);
 let finalTotal = calculateFinalTotal(taxes, subtotal, shipping);
 
 if(document.getElementById("subtotal-price-header")) {
@@ -17,3 +17,5 @@ if(document.getElementById("subtotal-price-header")) {
     document.getElementById("shipping-price-header").innerHTML = `Shipping: $${shipping}`;
     document.getElementById("total-price-header").innerHTML = `Total: $${finalTotal}`;
 }
+
+document.getElementById("hidden-price").setAttribute("opacity", 0);
