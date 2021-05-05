c2b04f634e840791ae441d8847aa127b4dbea83d
Adam <adam.j.menker@gmail.com>
2021-05-04 20:24:09 -0500

    dynamically generate orders: bug fixes

--- controllers/orders.js
+++ controllers/orders.js
@@ -44,23 +44,27 @@ exports.createOrder = (req, res, next) => {
 
 
 exports.getOrder = (req, res, next) => {
-    let userID = req.user.userID;
+    if(req.user.userID) {
+        let userID = req.user.userID;
 
-    db.query('SELECT * FROM orders WHERE userID = ?', [userID], async (error, result) => {
-        if(error) {
-            console.log(error);
+        db.query('SELECT * FROM orders WHERE userID = ?', [userID], async (error, result) => {
+            if(error) {
+                console.log(error);
+                return next();
+            } 
+
+            if(result.length == 0) {
+                return res.render('orderSummary', {
+                    message: 'You Have No Previous Orders'
+                });
+            } else {
+                req.orders = generateOrderHtml(result);
+            } 
             return next();
-        } 
-
-        if(result.length == 0) {
-            return res.render('orderSummary', {
-                message: 'You Have No Previous Orders'
-            });
-        } else {
-            req.orders = generateOrderHtml(result);
-        } 
-        return next();
-    });
+        });
+    } else {
+        next();
+    }
 }
 
 
