036fb8e22af70df3ebbe894bf78e48746b5e132e
Adam <adam.j.menker@gmail.com>
2021-05-04 21:20:11 -0500

    reversing the order of orders to show most recent at the top

--- public/generateOrders.js
+++ public/generateOrders.js
@@ -17,7 +17,7 @@ ordersArray = ordersText.slice(0, -1);
 
 function createTableRows(orderArr) {
     let tableBody = '';
-    for(let i = 0; i < orderArr.length; i++) {
+    for(let i = orderArr.length - 1; i != 0; i--) {
         currentOrder = orderArr[i].split(",");
         order_number = currentOrder[0];
         tracking_number = currentOrder[1];
