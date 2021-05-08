36a35d227c0d6ecdec476a5a93947b37d3926b2a
Adam <adam.j.menker@gmail.com>
2021-05-08 18:28:04 -0500

    allow removal of product from cart

--- public/generateCart.js
+++ public/generateCart.js
@@ -36,8 +36,8 @@ function createTableRows(productArr) {
                 `<td>${productCategory}</td>` +
                 `<td>${"$" + productPrice}</td>` +
                 `<td>
-                    <form id="remove-item-from-cart-form" action="/" method="POST" autocomplete="off">
-                        <button class="btn btn-outline-secondary" id="remove-item-from-cart-button" type="submit">
+                    <form id="remove-item-from-cart-form" action="/cart" method="POST" autocomplete="off">
+                        <button class="btn btn-outline-secondary" id="remove-item-from-cart-button" type="submit" onclick="removeItemRow(${productID})">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -84,3 +84,8 @@ document.getElementById("subtotal-label").innerHTML = `Subtotal: $${subtotal}`;
 document.getElementById("taxes-label").innerHTML = `Taxes: $${taxes}`;
 document.getElementById("shipping-label").innerHTML = `Shipping: $${shipping}`;
 document.getElementById("total-label").innerHTML = `Total: $${finalTotal}`;
+
+
+function removeItemRow(productID) {
+    console.log(productID);
+}
