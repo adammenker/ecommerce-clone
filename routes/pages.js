e637935329d28b87642966dcfcd40a2ff0cd22e0
Adam <adam.j.menker@gmail.com>
2021-05-09 18:18:49 -0500

    creating add to cart functionality on product page: testing

--- routes/pages.js
+++ routes/pages.js
@@ -103,17 +103,18 @@ router.post('/product', authController.isLoggedIn, productsController.getProduct
     }
 });
 
-// router.get('/product', authController.isLoggedIn, cartController.addToCart, (req, res) => {
-//     if(req.message){
-//         res.render('product', {
-//             message: req.message
-//         });
-//     } else {
-//         res.render('product', {
-//             user: req.user,
-//             product: req.product,
-//         });
-//     }
-// });
+router.get('/product', authController.isLoggedIn, cartController.addToCart, (req, res) => {
+    console.log('hit');
+    if(req.message){
+        res.render('product', {
+            message: req.message
+        });
+    } else {
+        res.render('product', {
+            user: req.user,
+            product: req.product,
+        });
+    }
+});
 
 module.exports = router;
