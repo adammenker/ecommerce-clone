1bc10a02a5c138b951b69dc417b1d3210c8fcd1f
Adam <adam.j.menker@gmail.com>
2021-05-09 18:37:00 -0500

    creating add to cart functionality on product page: testing

--- controllers/cart.js
+++ controllers/cart.js
@@ -97,6 +97,6 @@ exports.removeItemFromCart = (req, res, next) => {
 
 
 exports.addToCart = (req, res, next) => {
-    console.log(req.body.prodaddToCartButtonuct);
+    console.log(req.body.addToCartButton);
     return next();
 }
