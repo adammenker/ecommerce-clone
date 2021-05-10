b3952b413acf8dcd3a9efc882271f24561951727
Adam <adam.j.menker@gmail.com>
2021-05-09 19:22:26 -0500

    troubleshooting getting product value on addToCartButton

--- controllers/cart.js
+++ controllers/cart.js
@@ -97,16 +97,16 @@ exports.removeItemFromCart = (req, res, next) => {
 
 exports.addToCart = (req, res, next) => {
     try{
-        console.log(req.body);
         let product = (req.body.addToCartButton).split("**2Z$*4TZQ$**3");
-        req.product = {name: product[0],
-                    description: product[1],
-                    price: product[2],
-                    image: product[3]
-                    };
+        req.product = {name: product[0],description: product[1],price: product[2],image: product[3], productID: product[4]};
+
+
+
+        
+        return next();
     } catch(error){
         return next();
     }
     
-    return next();
+    
 }
