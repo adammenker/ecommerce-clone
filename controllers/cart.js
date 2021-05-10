17ccb3811451c6b73815598ed9af7e6913680a06
Adam <adam.j.menker@gmail.com>
2021-05-09 19:00:50 -0500

    troubleshooting getting product value on addToCartButton

--- controllers/cart.js
+++ controllers/cart.js
@@ -98,6 +98,7 @@ exports.removeItemFromCart = (req, res, next) => {
 
 exports.addToCart = (req, res, next) => {
     console.log(req.body);
+    console.log(req.body.addToCartButton);
     
     
     // console.log(req.body.addToCartButton);
