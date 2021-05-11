87a4cefa31d3a0ead3dfa1f8f146ccbd145b07c5
Adam <adam.j.menker@gmail.com>
2021-05-10 20:02:05 -0500

    fixing cart query bug

--- controllers/cart.js
+++ controllers/cart.js
@@ -35,48 +35,25 @@ exports.getCart = (req, res, next) => {
 
             // await getCartProductsArray(productIDsArray);
             let products = [];
-
-            (async function () {
-                for(let i = 0; i < productIDsArray.length; i++){
-                    console.log('** ' + i);
-                    let productID = productIDsArray[i];
-                    await db.query('SELECT * FROM products WHERE productID = ?', [productID], async (error, result) => {
-                        // console.log(result);
-                        console.log(i);
-                        products.push(result[0]);
-                        if(error) {
-                            console.log(error);
-                            return next();
-                        } 
-                        if(i == productIDsArray.length - 1){
-                            // productNames = generateCartHtml(products);
-                            req.products = generateCartHtml(products);
-                            console.log(req.products);
-                            return next();
-                        } 
-                    });
-                }
-            })();
-
-            // for(let i = 0; i < productIDsArray.length; i++){
-            //     console.log('** ' + i);
-            //     let productID = productIDsArray[i];
-            //     db.query('SELECT * FROM products WHERE productID = ?', [productID], async (error, result) => {
-            //         // console.log(result);
+            for(let i = 0; i < productIDsArray.length; i++){
+                console.log('** ' + i);
+                let productID = productIDsArray[i];
+                db.query('SELECT * FROM products WHERE productID = ?', [productID], async (error, result) => {
+                    // console.log(result);
                     
-            //         products.push(result[0]);
-            //         if(error) {
-            //             console.log(error);
-            //             return next();
-            //         } 
-            //         if(i == productIDsArray.length - 1){
-            //             // productNames = generateCartHtml(products);
-            //             req.products = generateCartHtml(products);
-            //             console.log(req.products);
-            //             return next();
-            //         } 
-            //     });
-            // }
+                    products.push(result[0]);
+                    if(error) {
+                        console.log(error);
+                        return next();
+                    } 
+                    if(i == productIDsArray.length - 1){
+                        // productNames = generateCartHtml(products);
+                        req.products = generateCartHtml(products);
+                        console.log(req.products);
+                        return next();
+                    } 
+                });
+            }
         });
     } else {
         return next();
