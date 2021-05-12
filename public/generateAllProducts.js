9b38b9420b3b272055c08eb02199375519c8d385
Adam <adam.j.menker@gmail.com>
2021-05-12 15:38:49 -0500

    generating all products

--- public/generateAllProducts.js
+++ public/generateAllProducts.js
@@ -18,27 +18,17 @@ console.log(allProductsArrays);
 function createProductWidgets(allProductsArrays) {
     let productName = '';
     let productImageSrc = '';
-    for(let i = 0; i < productArr.length; i++) {
-        currentProduct = productArr[i].split(",");
-        productID = currentProduct[0];
-        productName = (currentProduct[1]).replace("**2Z$*4TZQ$**3", ",");
-        productCategory = currentProduct[2];
-        productPrice = (parseFloat(currentProduct[3]).toFixed(2)).toString();
-        productPrice = 
-        tableBody += 
-            '<tr>' +
-                `<th scope="row">${productID}</th>` +
-                `<td>${productName}</td>` +
-                `<td>${productCategory}</td>` +
-                `<td>${"$" + productPrice}</td>` +
-                `<td>
-                    <form action="/cart" method="post" autocomplete="off">
-                        <button class="btn btn-outline-secondary" type="submit" name="removeItemProductId" value="${productID}">
-                            Remove
-                        </button>
-                    </form>
-                </td>` +
-            '</tr>'
+    let allProductsWidgets = '';
+    for(let i = 0; i < allProductsArrays.length; i++) {
+        productName = allProductsArrays[i][0];
+        productImageSrc = allProductsArrays[i][1];
+        allProductsWidgets += 
+            '<div>' +
+                `<img src="${productImageSrc}">${productID}</img>` +
+                `<p>${productName}</p>` +
+            '</div>'
     }
-    return tableBody;
+    return allProductsWidgets;
 }
+
+let imbeddedHTML = createProductWidgets(allProductsArrays);
