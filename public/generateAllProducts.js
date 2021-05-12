db8510c1f32044f6fa1fecc9f0f7a68a02478f80
Adam <adam.j.menker@gmail.com>
2021-05-12 16:04:16 -0500

    generating all products

--- public/generateAllProducts.js
+++ public/generateAllProducts.js
@@ -1,10 +1,11 @@
 let allProducts = document.getElementById("hidden-all-products-array").innerHTML;
 allProducts = allProducts.split("***23GA2e1SADF2***");
+allProducts[0] = "," + allProducts[0];
 allProducts.slice(0, -1);
 
 let allProductsArrays = [];
 for(let i = 0; i < allProducts.length - 1; i++) {
-    let currentProductName = allProducts[i];
+    let currentProductName = allProducts[i].slice(1);
     let currentProductImage = allProducts[i + 1];
     let currentProduct = [];
     currentProduct.push(currentProductName);
