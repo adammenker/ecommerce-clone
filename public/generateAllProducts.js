125b099f1fb965ef33c0a8d65b2515282170ef4d
Adam <adam.j.menker@gmail.com>
2021-05-12 15:32:03 -0500

    generating all products

--- public/generateAllProducts.js
+++ public/generateAllProducts.js
@@ -1,10 +1,9 @@
 let allProducts = document.getElementById("hidden-all-products-array").innerHTML;
-console.log(allProducts);
 allProducts = allProducts.split("\\");
 console.log("* " + allProducts);
 
 let allProductsArrays = [];
-for(let i = 0; i < allProducts; i++) {
+for(let i = 0; i < allProducts.length; i++) {
     let currentProductName = allProducts[i];
     let currentProductImage = allProducts[i + 1];
     let currentProduct = [];
@@ -14,3 +13,32 @@ for(let i = 0; i < allProducts; i++) {
     i++;
 }
 console.log(allProductsArrays);
+
+
+function createProductWidgets(allProductsArrays) {
+    let productName = '';
+    let productImageSrc = '';
+    for(let i = 0; i < productArr.length; i++) {
+        currentProduct = productArr[i].split(",");
+        productID = currentProduct[0];
+        productName = (currentProduct[1]).replace("**2Z$*4TZQ$**3", ",");
+        productCategory = currentProduct[2];
+        productPrice = (parseFloat(currentProduct[3]).toFixed(2)).toString();
+        productPrice = 
+        tableBody += 
+            '<tr>' +
+                `<th scope="row">${productID}</th>` +
+                `<td>${productName}</td>` +
+                `<td>${productCategory}</td>` +
+                `<td>${"$" + productPrice}</td>` +
+                `<td>
+                    <form action="/cart" method="post" autocomplete="off">
+                        <button class="btn btn-outline-secondary" type="submit" name="removeItemProductId" value="${productID}">
+                            Remove
+                        </button>
+                    </form>
+                </td>` +
+            '</tr>'
+    }
+    return tableBody;
+}
