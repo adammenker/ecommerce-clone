bbb10325871c69969e9024ec5b14a0c96038f213
Adam <adam.j.menker@gmail.com>
2021-05-12 15:28:13 -0500

    generating all products

--- public/generateAllProducts.js
+++ public/generateAllProducts.js
@@ -3,6 +3,13 @@ console.log(allProducts);
 allProducts = allProducts.split("\\");
 console.log("* " + allProducts);
 
+let allProductsArrays = [];
 for(let i = 0; i < allProducts; i++) {
-    
+    let currentProductName = allProducts[i];
+    let currentProductImage = allProducts[i + 1];
+    let currentProduct = [];
+    currentProduct.push(currentProductName);
+    currentProduct.push(currentProductImage);
+    allProductsArrays.push(currentProduct);
 }
+console.log(allProductsArrays);
