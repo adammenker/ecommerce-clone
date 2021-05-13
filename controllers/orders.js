72c76b976849c137c110f0db50a837939227ee3f
Adam <adam.j.menker@gmail.com>
2021-05-13 17:30:00 -0500

    maxing out connections

--- controllers/orders.js
+++ controllers/orders.js
@@ -54,12 +54,12 @@ exports.createOrder = (req, res, next) => {
             price = price.toFixed(2);
             console.log(price);
 
-            db.query('INSERT INTO payment SET ?', {card_number: creditCardNumber}, (error, results) => {
-                if(error) {
-                    console.log(error);
-                    return next();
-                }
-            });
+            // db.query('INSERT INTO payment SET ?', {card_number: creditCardNumber}, (error, results) => {
+            //     if(error) {
+            //         console.log(error);
+            //         return next();
+            //     }
+            // });
 
             db.query('INSERT INTO orders SET ?', {tracking_number: trackingNumber, order_date: date, ship_method: shippingCarrier, number_of_products: numberOfProducts, price: price, userID: userID}, (error, results) => {
                 if(error) {
