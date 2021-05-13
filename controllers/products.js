59ce739efc25a809df6f6315f7b80ef2682349a8
Adam <adam.j.menker@gmail.com>
2021-05-13 13:28:28 -0500

    passing clicked product to getProduct function

--- controllers/products.js
+++ controllers/products.js
@@ -11,7 +11,7 @@ const db = mysql.createPool({
 
 
 exports.getProduct = (req, res, next) => {
-    console.log(req.body);
+    console.log(req);
     let productName = req.body.userInput;
     db.query('SELECT * FROM products WHERE name = ?', [productName], async (error, result) => {
         if(error) {
